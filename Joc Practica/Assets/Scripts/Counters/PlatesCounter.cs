ea138ad2ab1ec2131b7ac35121d622b5bdd64e79
Ionut19 <ionutcosmiuc@yahoo.ro>
2023-06-19 13:09:59 +0300

    Adaugat PlatesCounter

---
+++ Joc Practica/Assets/Scripts/Counters/PlatesCounter.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlatesCounter : BaseCounter
+{
+    public event EventHandler OnPlateSpawned;
+    public event EventHandler OnPlateRemoved;
+
+
+    [SerializeField] private KitchenObjectSO plateKitchenObjectSO;
+    private float spawnPlateTimer;
+    private float spawnPlateTimerMax = 4f;
+    private int stackedPlates;
+    private int maxStackedPlates = 5;
+
+    private void Update()
+    {
+        spawnPlateTimer += Time.deltaTime;
+        
+        if(spawnPlateTimer > spawnPlateTimerMax&&stackedPlates<maxStackedPlates)
+        {
+            spawnPlateTimer= 0f;
+            stackedPlates++;
+            OnPlateSpawned?.Invoke(this,EventArgs.Empty);
+        }
+    }
+    public override void Interact(Player player)
+    {
+        if(!player.HasKitchenObject())
+        {
+            if(stackedPlates>0)
+            {
+                stackedPlates--;
+                KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, player);
+
+                OnPlateRemoved?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
