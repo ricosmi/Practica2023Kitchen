c189f7d367ed034b17d4c48459cd1bc1a26a28dd
Ionut19 <ionutcosmiuc@yahoo.ro>
2023-06-20 17:16:27 +0300

    Adaugat LookAtCamera pentru bara UI + Partea Vizuala Farfuri

--- Joc Practica/Assets/Scripts/PlateKitchenObject.cs
+++ Joc Practica/Assets/Scripts/PlateKitchenObject.cs
@@ -1,11 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+
+    public class OnIngredientAddedEventArgs : EventArgs
+    {
+        public KitchenObjectSO KitchenObjectSO;
+    }
     private List<KitchenObjectSO> kitchenObjectSOList;
     [SerializeField] private List<KitchenObjectSO> validkitchenObjectSOList;
+    
+    
 
     private void Awake()
     {
@@ -25,8 +34,16 @@ public class PlateKitchenObject : KitchenObject
         else
         {
             kitchenObjectSOList.Add(kitchenObjectSO);
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs
+            {
+                KitchenObjectSO = kitchenObjectSO
+            });
             return true;
         }
 
+    }
+    public List<KitchenObjectSO> GetKitchenObjectSOList()
+    {
+        return kitchenObjectSOList;
     }
 }
